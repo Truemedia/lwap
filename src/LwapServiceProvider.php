7c29cc86cec4a4f596faea7d9f9ff504260aa548
Wade Penistone <wadespackages@yahoo.com>
2017-05-27 20:18:48 +0100

    Boilerplate PSR-4

---
+++ src/LwapServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace Truemedia\Lwap\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LwapServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
